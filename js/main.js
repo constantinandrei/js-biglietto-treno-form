c05b61108c3c4bab92a938476854fecd263a189d
constantinandrei <andrei.todirascu@gmail.com>
2022-05-19 16:47:31 +0200

    problem

--- js/main.js
+++ js/main.js
@@ -30,38 +30,48 @@ const discount20 = 0.2;
 const discount40 = 0.4;
 
 let finalPrice;
+
 // raccolta dati dal form
 
 const inputUserCompleteName = document.getElementById("nome-cognome");
 const inputKm = document.getElementById("km-da-percorrere");
 const inputAgeRange = document.getElementById('fascia-di-eta');
 // validazione dei dati inseriti
-
 let dataTypeOk = true;
 
 
 
 // calcolo dati del biglieto random
-
-// display dati nel form
-
-
+let offert = 'Standard';
 
 // funzionamento pulsanti del form
 
 buttonCreateTicket.addEventListener('click', function () {
 
     if (dataTypeOk) {
+
+        // calcolo prezzo del biglietto
+
         finalPrice = priceForKm * inputKm.value;
         if (inputAgeRange.value === 'discount-20') {
             finalPrice -= finalPrice * discount20;
+            offert = "Sconto 20%";
         }
     
         if (inputAgeRange.value === 'discount-40') {
             finalPrice -= finalPrice * discount40;
+            offert = "Sconto 40%";
         }
 
         finalPrice = finalPrice.toFixed(2);
+
+        const passengerName = document.getElementById('nome-passegero');
+        const tiketOffert = document.getElementById('offerta');
+        const carriage = document.getElementById('carrozza');
+        const ticketPrice = document.getElementById('costo-biglietto');
+
+        passengerName.innerHTML = inputUserCompleteName.textContent;
+        ticketPrice.innerHTML = finalPrice;
     }
 
 
@@ -69,4 +79,4 @@ buttonCreateTicket.addEventListener('click', function () {
     
 })
 
-// calcolo prezzo del biglietto
+// display dati nel form

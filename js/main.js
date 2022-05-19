b5399d040012b137b7deb594fffb2ca839afc13f
constantinandrei <andrei.todirascu@gmail.com>
2022-05-19 18:39:59 +0200

    esercizio competato con alcuni bonus

--- js/main.js
+++ js/main.js
@@ -26,6 +26,10 @@ il responsive è opzionale!!!!
 const buttonCreateTicket = document.getElementById('create-ticket');
 const cancelButton = document.getElementById('cancel');
 const generatedTicket = document.getElementById('generated-ticket')
+// variabili per messaggi errore
+const nameError = document.getElementById('name-error-message');
+const kmError = document.getElementById('km-error-message');
+const ageRangeError = document.getElementById('age-error-message');
 
 const priceForKm = 0.21;
 const discount20 = 0.2;
@@ -40,15 +44,36 @@ const inputKm = document.getElementById("km-da-percorrere");
 const inputAgeRange = document.getElementById('fascia-di-eta');
 
 
-// validazione dei dati inseriti
-let dataTypeOk = true;
+
 
 // calcolo dati del biglieto random
 let offert = 'Standard';
 
+let carriageNumber = Math.ceil(Math.random()* 9 + 1);
+let cpCodeNumber = Math.ceil(Math.random()* 10000 + 1)
 // funzionamento pulsanti del form
 
 buttonCreateTicket.addEventListener('click', function () {
+
+        // validazione dei dati inseriti
+    let dataTypeOk = true;
+    if (inputUserCompleteName.value === ''){
+        dataTypeOk = false;
+    } else {
+        nameError.innerHTML = '';
+    }
+
+    if (inputKm.value < 0) {
+        dataTypeOk = false;
+    } else {
+        kmError.innerHTML = '';
+    }
+
+    if (inputAgeRange.value === '') {
+        dataTypeOk = false;
+    } else {
+        ageRangeError.innerHTML = '';
+    }
 
     if (dataTypeOk) {
 
@@ -83,9 +108,32 @@ buttonCreateTicket.addEventListener('click', function () {
 
         passengerName.innerHTML = inputUserCompleteName.value;
         tiketOffert.innerHTML = offert;
-        carriage.innerHTML = "";
-        cpCode.innerHTML = "";
+        carriage.innerHTML = carriageNumber;
+        cpCode.innerHTML = cpCodeNumber;
         ticketPrice.innerHTML = '€ ' + finalPrice;
+    } else {
+        
+
+        if (inputUserCompleteName.value === ''){
+            nameError.innerHTML = 'Inserire il vostro nome';
+            if (!generatedTicket.classList.contains('d-none')) {
+                generatedTicket.classList.add('d-none');
+            }
+        }
+    
+        if (inputKm.value < 0 || inputKm.value === '') {
+            kmError.innerHTML = 'Inserire un numero positivo';
+            if (!generatedTicket.classList.contains('d-none')) {
+                generatedTicket.classList.add('d-none');
+            }
+        }
+    
+        if (inputAgeRange.value === '') {
+            ageRangeError.innerHTML = 'Selezionare una fascia di eta';
+            if (!generatedTicket.classList.contains('d-none')) {
+                generatedTicket.classList.add('d-none');
+            }
+        }
     }
 
 
@@ -101,6 +149,10 @@ cancelButton.addEventListener('click', function () {
     inputAgeRange.value = '';
     inputUserCompleteName.value = '';
     inputKm.value = '';
+
+    kmError.innerHTML = '';
+    nameError.innerHTML = '';
+    ageRangeError.innerHTML = '';
 })
 
 
